aecc4a6764127be62008808de0796204125e1e57
Paul <paul.ngugi@student.moringaschool.com>
2023-02-21 12:24:35 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,9 @@
 class Dog
 
     attr_accessor :name, :breed, :id
+    # attr_reader :id
 
-    def initialize(name:, breed:, id: nil)
+    def initialize(id: nil, name:, breed:)
         @id =id
         @name =name
         @breed = breed
@@ -25,6 +26,9 @@ class Dog
     end
 
     def save
+        if self.id
+            self.update
+        else
         sql = <<-SQL
             INSERT INTO dogs (name, breed)
             VALUES (?, ?)
@@ -33,13 +37,15 @@ class Dog
         DB[:conn].execute(sql, self.name, self.breed)
 
         self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-
+        # @id =      ""
         self
+        end
     end
 
     def self.create(name:, breed:)
         dog = Dog.new(name: name, breed: breed)
         dog.save
+        # dog
     end
 
     def self.new_from_db(row)
@@ -80,6 +86,22 @@ class Dog
         DB[:conn].execute(sql, id).map do |row|
             self.new_from_db(row)
         end.first
+    end
+
+    def self.find_or_create_by(name:, breed:)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+        if !dog.empty?
+            dog_data = dog[0]
+            dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+        else
+            dog = self.create(name: name, breed: breed)
+        end
+        dog
+    end
+
+    def update
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
     end
 
 end
